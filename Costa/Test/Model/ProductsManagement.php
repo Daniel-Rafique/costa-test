d8ce54981e31663ece70bf312ca5a33e183427d5
Daniel Rafique - Black <danielrafique-black@Daniels-MacBook-Pro.local>
2017-11-15 16:20:59 +0000

    Webservice method

--- Costa/Test/Model/ProductsManagement.php
+++ Costa/Test/Model/ProductsManagement.php
@@ -1,15 +1,4 @@
 <?php
-/**
- *  *
- *  * Shopcade Marketplace extension
- *  * Copyright (C) 2017.
- *  *
- *  * This file included in Gravytrain/Ageverification is licensed under OSL 3.0
- *  *
- *  * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *  * Please see LICENSE.txt for the full text of the OSL 3.0 license
- *
- */
 
 /**
  * A web service method for Magento 2 Community Edition that will return a product with all of its child sku options in a single response.
